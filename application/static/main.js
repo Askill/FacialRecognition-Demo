7313fbd58d100aaba05b79564452e2598237d31a
Askill <ofjokg@gmail.com>
2020-03-16 19:51:17 +0100

    more layout

--- application/static/main.js
+++ application/static/main.js
@@ -1,137 +1,70 @@
-var rootKontext = "/od1";
-//var rootKontext = "";
-var customeStyle = false;
 
-function loadOD1(){
-    try{
-        let ksession = $.cookie("KSESSIONID");
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", rootKontext, false );
-        xmlHttp.send();
-        document.getElementById("od1").innerHTML = xmlHttp.responseText;
+rootKontext = ""
 
-        $('link[title="custom"]').prop('disabled', true);
-
-        loadData();
-    }
-    catch(e){
-        document.getElementById("od1").innerHTML = "Ein Fehler ist beim Laden des Dienstes aufgetreten, bitte versuchen Sie es später erneut.";
-    }
-}
-
-function nextTab(){
-    $('#navMenu1 a').removeClass("disabled");
-    $('#navMenu1 a').tab('show');
-}
-
-function getJSON (url, callback) {
-    let ksession = $.cookie("KSESSIONID");
-
+function getJSON(url, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url + "?session_id=" + ksession, true);
+    xhr.open('GET', url, true);
     xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status < 400) {
-        callback(null, xhr.response);
-      } else {
-        console.log("failed getting");
-        console.log(status);
-      }
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status < 400) {
+            callback(null, xhr.response);
+        } else {
+            console.log("failed getting");
+            console.log(status);
+        }
     };
     xhr.send();
 };
 
-function putJSON (url, data, callback) {
-    let ksession = $.cookie("KSESSIONID");
-
+function putJSON(url, data, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open('PUT', url + "?session_id=" + ksession, true);
+    xhr.open('PUT', url, true);
     xhr.responseType = 'json';
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status < 400) {
-        callback(null, xhr.response);
-      } else {
-        console.log("failed posting");
-        console.log(status);
-      }
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status < 400) {
+            callback(null, xhr.response);
+        } else {
+            console.log("failed posting");
+            console.log(status);
+        }
     };
     xhr.send(data);
 };
 
-function sendVorhaben(){
-    let data = document.getElementById("form1");
 
-    data = {};
-    data["name"] = document.getElementById("form1")["name"].value;
-    data["reason"] = document.getElementById("form1")["reason"].value;
-    data["note"] = document.getElementById("form1")["note"].value;
-    data["partial_application_id"] = document.getElementById("form1")["partial_application_id"].value;
-
-    putJSON(rootKontext + "/api/v1/vorhaben/" + data["partial_application_id"],  JSON.stringify(data), function(error, data){
-        document.getElementById("status").innerHTML = "Daten erfolgreich erneuert";
-        loadData();
-        nextTab();
-    });
-
-}
-
-function queryString(string)
-{
-    let queryString = window.location.search;
-    let varArray = queryString.split("&");
-    for (let i=0;i<varArray.length;i++) {
-      let param = varArray[i].split("=");
-        if(param[0] == string ){
-            return param[1]
-        }
-    }
-}
-
-function loadData(){
-    let encodedID = queryString("?partialApplicationId");
-    //encodedID = "d05e6d3a-1774-46a0-abb0-89abfe2ff5a3";
-   getJSON(rootKontext + "/api/v1/vorhaben/" + encodedID, 
-        function(error, data){
+function loadData() {
+    getJSON(rootKontext + "/api/v1/person/",
+        function (error, data) {
             console.log(data)
-            populateForm(data["data"], "form1")
-            populateForm(data["data"], "form2")
+            data = data["data"]
+            let el = document.getElementById('list');
+            data.forEach(function (item) {
+                string = `
+                <div class="card border-light">
+                    <div class="card-body">
+                        <h4 class="card-title">${item["fname"]} ${item["lname"]}</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">${item["timestamp"]}</h6>
+
+                        <p class="card-text container">
+                            <img class="listImg" src="${item["face"]}"></img>
+                            <div class="personalInfo">
+                                ${item["gender"]} <br>
+                                ${item["yob"]} <br>
+                                ${item["fingerprints"].length} <br>
+                            </div>
+                            
+                        </p>
+                    </div>
+                </div>
+                `
+                el.innerHTML += string;
+            })
+
+
+
         }
     );
-}
-
-function populateForm(data, formId){
-    console.log(data)
-    inputs = document.forms[formId];
-
-    for(var i = 0; i < 8; i++){   
-        if(inputs[i] !== undefined && data[inputs[i].name] !== undefined){
-            inputs[i].value = data[inputs[i].name];
-        }
-        else{
-            console.log(inputs);
-        }
-    }
-}
-
-function loadNewStyle(){
-    customeStyle = !customeStyle; 
-
-    if(customeStyle){
-        $('link[title="custom"]').prop('disabled', false);
-        $('link[title="default"]').prop('disabled', true);
-
-        console.log("StyleSheet: Custom");
-        alert("StyleSheet: Custom");
-    }
-    else{
-        $('link[title="default"]').prop('disabled', false);
-        $('link[title="custom"]').prop('disabled', true);
-
-        console.log("StyleSheet: Default");
-        alert("StyleSheet: Default");
-    }
-
 }
